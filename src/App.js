5ab7d7bc7993155c8f37e7f6794389a2293c6203
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-30 10:40:23 +1100

    Add state for App

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import './App.css'
 import StockInfo from './components/StockInfo'
 
 class App extends Component {
+  state = {
+    quote: {
+      symbol: 'AAPL',
+      companyName: 'Apple Inc.',
+      primaryExchange: 'Nasdaq Global Select',
+      latestPrice: 169.48,
+      latestSource: 'Close',
+      week52High: 176.24,
+      week52Low: 108.25,
+    }
+  }
+
   render() {
+    const { quote } = this.state
+
     return (
       <div className="App">
         <h1>React Stock Market</h1>
         <StockInfo
-          symbol='AAPL'
-          companyName='Apple Inc.'
-          primaryExchange='Nasdaq Global Select'
-          latestPrice={ 169.48 }
-          latestSource='Close'
-          week52High={ 176.24 }
-          week52Low={ 108.25 }
+          { ...quote }
         />
       </div>
     )
