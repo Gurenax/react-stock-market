82fe2c61a02e22bb2990f2539eca5528d63d1919
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-30 10:11:56 +1100

    Initialise repo with create-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React Stock Market</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
