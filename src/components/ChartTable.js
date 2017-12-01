cbc96169c6f756932e77e84b1ddd47810a6d1c03
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-01 11:53:57 +1100

    Remove unused chartLength variable in ChartTable component

--- src/components/ChartTable.js
+++ src/components/ChartTable.js
@@ -1,32 +1,29 @@
 import React from 'react'
 import ChartItem from './ChartItem'
 
-const ChartTable = ({ chart }) => {
-  const chartLength = chart.length
-  return (
-    <div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Date</th>
-            <th scope="col">Open</th>
-            <th scope="col">High</th>
-            <th scope="col">Low</th>
-            <th scope="col">Close</th>
-          </tr>
-        </thead>
-        <tbody>
-          {chart.map((chartItem, index) => {
-            return chartItem.change < 0 ? (
-              <ChartItem key={'chartTable' + index} {...chartItem} stockIsUp />
-            ) : (
-              <ChartItem key={'chartTable' + index} {...chartItem} />
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+const ChartTable = ({ chart }) => (
+  <div>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Open</th>
+          <th scope="col">High</th>
+          <th scope="col">Low</th>
+          <th scope="col">Close</th>
+        </tr>
+      </thead>
+      <tbody>
+        {chart.map((chartItem, index) => {
+          return chartItem.change < 0 ? (
+            <ChartItem key={'chartTable' + index} {...chartItem} stockIsUp />
+          ) : (
+            <ChartItem key={'chartTable' + index} {...chartItem} />
+          )
+        })}
+      </tbody>
+    </table>
+  </div>
+)
 
 export default ChartTable
